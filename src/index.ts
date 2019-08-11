95928e828b90130d3264051621de1d090c373afb
Marcel van den Hof <854060+marcelvdh@users.noreply.github.com>
2019-08-11 12:46:18 +0100

    :sparkles: first commit

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { isPrimeNumber } from "./prime";
+
+const n = Math.floor(Math.random() * 1000);
+if (isPrimeNumber(n)) {
+  console.log(`The number ${n} is a prime number.`);
+} else {
+  console.log(`The number ${n} is not a prime number.`);
+}
---
+++ test/prime.test.ts
@@ -0,0 +1,11 @@
+import { isPrimeNumber } from "../src/prime";
+
+describe("Test prime module", (): void => {
+  it("Check if number is correctly reported as prime number", (): void => {
+    expect(isPrimeNumber(2)).toBe(true);
+  });
+
+  it("Check if number is reported as not a prime number", (): void => {
+    expect(isPrimeNumber(3)).toBe(true);
+  });
+});
